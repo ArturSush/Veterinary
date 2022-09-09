ed340cecebe4f91b2458a707a2c4a37059a38b9f
ArturSush <104769052+ArturSush@users.noreply.github.com>
2022-09-09 15:40:59 +0300

    Delete app directory

--- app/src/main/java/repository/AnimalRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.Animal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnimalRepository extends JpaRepository<Animal, Integer> {
-}
